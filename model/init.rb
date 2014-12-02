f37ad2a19242e94335772ad2981af1e50b3661c4
Jacky Shen <jacshen@ebay.com>
2014-12-01 21:35:06 -0800

    user session

--- model/init.rb
+++ model/init.rb
@@ -13,11 +13,11 @@ c_dir = File.dirname(__FILE__)
 db_dir = c_dir + "/../db"
 
 configure :development do
-  DataMapper::setup(:default, "sqlite3://#{db_dir}/bookmarks.db")
+  DataMapper::setup(:default, "sqlite3://#{db_dir}/bookmarksh.db")
 end
 
 configure :production do
-  DataMapper::setup(:default, "postgres://bjjtxhuqcemetk:qapOhCoSMPmrRp8kVYFRgI1eiP@ec2-54-221-206-165.compute-1.amazonaws.com:5432/d9mkc83pd75vnk")
+  DataMapper::setup(:default, "postgres://trfoajmntjazdy:ufLWnpJfMlDygGunJ5YdlpVJwA@ec2-54-243-51-102.compute-1.amazonaws.com:5432/dbibra39e733iq")
 end
 
 DataMapper.finalize.auto_upgrade!
